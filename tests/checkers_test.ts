4041b981a40bdcb4f5e7934c50b99126a67e21b4
morelucks <luckykamshak@gmail.com>
2026-04-12 08:11:02 +0100

    test(move): player1 piece promotes to king on reaching row 8

--- tests/checkers_test.ts
+++ tests/checkers_test.ts
@@ -392,3 +392,29 @@ Clarinet.test({
     midPiece.result.expectUint(0); // captured piece should be gone
   },
 });
+
+Clarinet.test({
+  name: "move: player1 piece promotes to king when reaching row 8 (pos >= 56)",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const player1 = accounts.get("wallet_1")!;
+    const player2 = accounts.get("wallet_2")!;
+    chain.mineBlock([
+      Tx.contractCall("checkers", "create-game", [], player1.address),
+      Tx.contractCall("checkers", "join-game", [types.uint(0)], player2.address),
+    ]);
+    // Manually simulate: place p1 piece near promotion row by a series of moves
+    // p1: 17->26, p2: 49->42, p1: 26->35, p2: 42->35? no - use 42->33
+    // Simpler: p1: 23->30, p2: 40->33, p1: 30->37, p2: 33->26, p1: 37->44, p2: 26->19, p1: 44->51, p2: 19->12, p1: 51->58
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(23), types.uint(30)], player1.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(40), types.uint(33)], player2.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(30), types.uint(37)], player1.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(33), types.uint(26)], player2.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(37), types.uint(44)], player1.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(26), types.uint(19)], player2.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(44), types.uint(51)], player1.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(19), types.uint(12)], player2.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(51), types.uint(58)], player1.address)]);
+    const piece = chain.callReadOnlyFn("checkers", "get-piece", [types.uint(0), types.uint(58)], player1.address);
+    piece.result.expectUint(2); // promoted to king (u2)
+  },
+});
