1b4166f0b165aafbc2912facc8297a6c2bcba35a
morelucks <luckykamshak@gmail.com>
2026-04-12 08:02:35 +0100

    test(create-game): game nonce increments for each new game created

--- tests/checkers_test.ts
+++ tests/checkers_test.ts
@@ -15,3 +15,17 @@ Clarinet.test({
     block.receipts[0].result.expectOk().expectUint(0);
   },
 });
+
+Clarinet.test({
+  name: "create-game: game nonce increments with each new game",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const player1 = accounts.get("wallet_1")!;
+    const player2 = accounts.get("wallet_2")!;
+    const block = chain.mineBlock([
+      Tx.contractCall("checkers", "create-game", [], player1.address),
+      Tx.contractCall("checkers", "create-game", [], player2.address),
+    ]);
+    block.receipts[0].result.expectOk().expectUint(0);
+    block.receipts[1].result.expectOk().expectUint(1);
+  },
+});
