4eef0b42477733b0c16b0bcdce95af0d26d1678e
morelucks <luckykamshak@gmail.com>
2026-04-12 08:02:01 +0100

    test: add section header for create-game tests

--- tests/checkers_test.ts
+++ tests/checkers_test.ts
@@ -1,2 +1,6 @@
 import { Clarinet, Tx, Chain, Account, types } from "https://deno.land/x/clarinet@v1.7.1/index.ts";
 import { assertEquals } from "https://deno.land/std@0.170.0/testing/asserts.ts";
+
+// ============================================================
+// create-game tests
+// ============================================================
