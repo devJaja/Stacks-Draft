59c6e10f378ea78f3fe6007fc4d2544e4b897f2d
Dev Jaja <jajak5242@gmail.com>
2026-04-14 02:17:28 -0400

    feat: extract contract config into frontend/constants.ts

---
+++ frontend/constants.ts
@@ -0,0 +1,4 @@
+export const CONTRACT_ADDRESS = 'SP2DWWDVSSKZ5X37BBV3RV0GY0A0FFZZESYHEVQZ9';
+export const CONTRACT_NAME = 'checkers';
+export const NETWORK = 'mainnet';
+export const EXPLORER_URL = 'https://explorer.hiro.so';
