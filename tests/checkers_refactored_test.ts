18137c158e361dae1d5df3639aa6aeacf3b0eea0
morelucks <luckykamshak@gmail.com>
2026-04-15 08:26:51 +0100

    test(create-game): player2 and winner are none on creation

--- tests/checkers_refactored_test.ts
+++ tests/checkers_refactored_test.ts
@@ -69,3 +69,15 @@ Clarinet.test({
     assertEquals(data["current-turn"], p1.address);
   },
 });
+
+Clarinet.test({
+  name: "create-game: player2 and winner are none on creation",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const p1 = accounts.get("wallet_1")!;
+    chain.mineBlock([Tx.contractCall("checkers", "create-game", [], p1.address)]);
+    const data = chain.callReadOnlyFn("checkers", "get-game", [types.uint(0)], p1.address)
+      .result.expectSome().expectTuple();
+    data["player2"].expectNone();
+    data["winner"].expectNone();
+  },
+});
