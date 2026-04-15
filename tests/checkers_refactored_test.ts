ef307ac71edc4269a7cbf40806a9a51aa1638e2d
morelucks <luckykamshak@gmail.com>
2026-04-15 08:19:31 +0100

    test: scaffold refactored checkers test file with Clarinet and assert imports

---
+++ tests/checkers_refactored_test.ts
@@ -0,0 +1,2 @@
+import { Clarinet, Tx, Chain, Account, types } from "https://deno.land/x/clarinet@v1.7.1/index.ts";
+import { assertEquals, assertNotEquals } from "https://deno.land/std@0.170.0/testing/asserts.ts";
