e29a701e69b3e6d287f4acebde6fa151398e6d1a
morelucks <luckykamshak@gmail.com>
2026-04-15 08:41:48 +0100

    test(move): p2 piece promotes to piece-p2-king on reaching promotion-row-p2

--- tests/checkers_refactored_test.ts
+++ tests/checkers_refactored_test.ts
@@ -370,3 +370,22 @@ Clarinet.test({
       .result.expectUint(2);
   },
 });
+
+Clarinet.test({
+  name: "move: p2 piece promotes to piece-p2-king (u4) on reaching promotion-row-p2",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const { p1, p2 } = setupActiveGame(chain, accounts);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(17), types.uint(24)], p1.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(40), types.uint(33)], p2.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(24), types.uint(31)], p1.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(33), types.uint(26)], p2.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(31), types.uint(38)], p1.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(26), types.uint(19)], p2.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(38), types.uint(45)], p1.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(19), types.uint(12)], p2.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(45), types.uint(52)], p1.address)]);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(12), types.uint(5)], p2.address)]);
+    chain.callReadOnlyFn("checkers", "get-piece", [types.uint(0), types.uint(5)], p2.address)
+      .result.expectUint(4);
+  },
+});
