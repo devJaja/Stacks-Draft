7e3e349fd66d14db9afe66ea6c171c26e3db13f7
morelucks <luckykamshak@gmail.com>
2026-05-16 17:56:50 +0100

    test(stacks): create-game initializes Stacks board with p2 pieces on row 6

--- tests/checkers_stacks_test.ts
+++ tests/checkers_stacks_test.ts
@@ -104,3 +104,15 @@ Clarinet.test({
     }
   },
 });
+
+Clarinet.test({
+  name: "[stacks] create-game: Stacks board initializes p2 pieces at positions 40,42,44,46",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const p1 = accounts.get("wallet_1")!;
+    chain.mineBlock([Tx.contractCall("checkers", "create-game", [], p1.address)]);
+    for (const pos of [40, 42, 44, 46]) {
+      chain.callReadOnlyFn("checkers", "get-piece", [types.uint(0), types.uint(pos)], p1.address)
+        .result.expectUint(3);
+    }
+  },
+});
