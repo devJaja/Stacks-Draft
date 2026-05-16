5164ccf8a999cf7d0c4fff8ccf815d42f1461211
morelucks <luckykamshak@gmail.com>
2026-05-16 18:37:58 +0100

    test(stacks): move Stacks player2 makes valid diagonal move after player1

--- tests/checkers_stacks_test.ts
+++ tests/checkers_stacks_test.ts
@@ -676,3 +676,15 @@ Clarinet.test({
     assertEquals(d1["player1"], p2.address);
   },
 });
+
+Clarinet.test({
+  name: "[stacks] move: Stacks player2 makes valid diagonal move after player1",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const { p1, p2 } = activeGame(chain, accounts);
+    chain.mineBlock([Tx.contractCall("checkers", "move", [types.uint(0), types.uint(17), types.uint(24)], p1.address)]);
+    const block = chain.mineBlock([
+      Tx.contractCall("checkers", "move", [types.uint(0), types.uint(40), types.uint(33)], p2.address),
+    ]);
+    block.receipts[0].result.expectOk().expectBool(true);
+  },
+});
