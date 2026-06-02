c1a0808c93fbb9f776da65b4bf45f09c56ab24ad
devJaja <devJaja@users.noreply.github.com>
2026-06-02 12:11:49 -0400

    feat(frontend): add LEADERBOARD_CONTRACT_NAME and POLL_INTERVAL to constants

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,4 +1,6 @@
 export const CONTRACT_ADDRESS = 'SP2DWWDVSSKZ5X37BBV3RV0GY0A0FFZZESYHEVQZ9';
 export const CONTRACT_NAME = 'checkers';
+export const LEADERBOARD_CONTRACT_NAME = 'checkers-leaderboard';
 export const NETWORK = 'mainnet';
 export const EXPLORER_URL = 'https://explorer.hiro.so';
+export const POLL_INTERVAL = 5000;
